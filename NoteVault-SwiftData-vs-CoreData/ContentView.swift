07549413aabe4e0c2a8cd67e9c0dbdfef635e2d6
Sachindra Fernando <sachindrafernando3@gmail.com>
2025-07-16 22:46:27 +0100

    Initial Commit

---
+++ NoteVault-SwiftData-vs-CoreData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoteVault-SwiftData-vs-CoreData
+//
+//  Created by Sachindra Fernando on 2025-07-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoteVault-SwiftData-vs-CoreData/NoteVault_SwiftData_vs_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteVault_SwiftData_vs_CoreDataApp.swift
+//  NoteVault-SwiftData-vs-CoreData
+//
+//  Created by Sachindra Fernando on 2025-07-16.
+//
+
+import SwiftUI
+
+@main
+struct NoteVault_SwiftData_vs_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
